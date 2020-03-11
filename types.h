15e9ceb3258624b9363aef1ae0b972e9426ec543
Raido Pahtma <raidopahtma@gmail.com>
2020-03-11 16:58:21 +0200

    Use standard types.

--- types.h
+++ types.h
@@ -2,6 +2,7 @@
 #define TYPES_H_
 
 #include <stdlib.h> // NULL
+#include <stdint.h>
 #include <math.h>
 
 // error_t values - not sure if they match up with TOS
@@ -14,11 +15,5 @@
 #define TRUE true
 
 typedef int error_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
 
 #endif /*TYPES_H_*/
